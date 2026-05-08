211199d5201f4aadb726c98f506de1d86a648b00
ayoubelhilali <elhilaliayoub2020@gmail.com>
2026-05-08 18:07:12 +0100

    bookDAO completed with search and update functions and BookService created

--- backend/src/main/java/com/library/Main.java
+++ backend/src/main/java/com/library/Main.java
@@ -1,16 +1,23 @@
 package com.library;
 
 import com.library.dao.BookDAO;
+import com.library.model.Book;
 import com.library.patterns.singleton.DBConnection;
 
 public class Main {
     public static void main(String[] args) {
         DBConnection db = DBConnection.getInstance();
-        System.out.println(db.getConnection());
         BookDAO dao = new BookDAO();
-
         dao.getAllBooks().forEach(book ->
                 System.out.println(book.getTitle())
         );
+        Book book=dao.getBookById(1);
+        book.setTitle("clean code updated");
+        dao.updateBook(book);
+        System.out.println(book.getId()+"=> "+book.getTitle());
+        dao.searchBooks("Software")
+                .forEach(elem ->
+                        System.out.println(elem.getTitle())
+                );
     }
 }
--- backend/src/main/java/com/library/dao/BookDAO.java
+++ backend/src/main/java/com/library/dao/BookDAO.java
@@ -20,17 +20,7 @@ public class BookDAO {
                 Statement stmt=connection.createStatement();
                 ResultSet rs=stmt.executeQuery(query)
                 ) {
-                while (rs.next()){
-                    Book book=new Book();
-                    book.setId(rs.getInt("id"));
-                    book.setTitle(rs.getString("title"));
-                    book.setAuthor(rs.getString("author"));
-                    book.setCategory(rs.getString("category"));
-                    book.setIsbn(rs.getString("isbn"));
-                    book.setStatus(rs.getString("status"));
-
-                    books.add(book);
-                }
+            fetchResult(books, rs);
         }catch (SQLException e){
             e.printStackTrace();
         }
@@ -62,5 +52,83 @@ public class BookDAO {
             e.printStackTrace();
         }
         return false;
+    }
+    public Book getBookById(int id){
+        String query="SELECT * FROM books WHERE id=?";
+        try(
+                PreparedStatement ps=connection.prepareStatement(query);
+                ){
+            ps.setInt(1,id);
+            ResultSet rs=ps.executeQuery();
+            if (rs.next()){
+                Book book=new Book();
+                book.setId(rs.getInt("id"));
+                book.setTitle(rs.getString("title"));
+                book.setAuthor(rs.getString("author"));
+                book.setCategory(rs.getString("category"));
+                book.setIsbn(rs.getString("isbn"));
+                book.setStatus(rs.getString("status"));
+                return book;
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public Boolean updateBook(Book book){
+        String query ="UPDATE books SET title=?,author=?,category=?,isbn=?,status=? WHERE id=?";
+        try(PreparedStatement ps=connection.prepareStatement(query)){
+            ps.setString(1,book.getTitle());
+            ps.setString(2,book.getAuthor());
+            ps.setString(3,book.getCategory());
+            ps.setString(4,book.getIsbn());
+            ps.setString(5,book.getStatus());
+            ps.setInt(6,book.getId());
+            int rows=ps.executeUpdate();
+            return rows>0;
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public List<Book> searchBooks(String keyword) {
+        List<Book> books = new ArrayList<>();
+        String query = """
+            SELECT * FROM books
+            WHERE title LIKE ?
+            OR author LIKE ?
+            OR category LIKE ?
+            """;
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+
+            String search = "%" + keyword + "%";
+
+            ps.setString(1, search);
+            ps.setString(2, search);
+            ps.setString(3, search);
+
+            ResultSet rs = ps.executeQuery();
+            fetchResult(books, rs);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return books;
+    }
+
+    private void fetchResult(List<Book> books, ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            Book book = new Book();
+            book.setId(rs.getInt("id"));
+            book.setTitle(rs.getString("title"));
+            book.setAuthor(rs.getString("author"));
+            book.setCategory(rs.getString("category"));
+            book.setIsbn(rs.getString("isbn"));
+            book.setStatus(rs.getString("status"));
+            books.add(book);
+        }
     }
 }
